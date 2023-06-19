e384867dd16b411bc58113d06a990bd464ecdb6d
@syabdulr <abdulsyed1998@gmail.com>
2023-06-19 15:28:29 -0400

    fix routes again

--- client/src/components/Pagination/styles.js
+++
@@ -1,8 +0,0 @@
-// Material-UI
-import { makeStyles } from "@material-ui/core/styles";
-
-export default makeStyles(() => ({
-  ul: {
-    justifyContent: "space-around",
-  },
-}));
--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-// Reducers
-import posts from "./posts";
-import auth from "./auth";
-
-export default combineReducers({ posts, auth });
